5191111f6fbb34b6c3dc5530ca8fe7287b72d058
Quentin Ikeno <ikenoq@gmail.com>
2023-05-18 18:42:11 -1000

    Add return type to add method and keys to object.

--- services/equations.py
+++ services/equations.py
@@ -3,14 +3,21 @@ from random import randint
 defaultMax = 99
 defaultMin = 1
 
+class Equation:
+    first: int
+    second: int
+    operation: str
+    expression: str
+    answer: int
+
 class Equations:
     def __init__(self, maxFirst = defaultMax, minFirst = defaultMin, maxSecond = defaultMax, minSecond = defaultMin, negative = False):
         self.first = randint(minFirst, maxFirst)
         self.second = randint(minSecond, maxSecond)
         self.negative = negative
 
-    def add(self):
+    def add(self) -> Equation:
         operation = "+"
-        expression = f"{self.first} {self.operation} {self.second}"
+        expression = f"{self.first} {operation} {self.second}"
         answer = self.first + self.second
-        return {self.first, self.second, operation, expression, answer}
+        return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
