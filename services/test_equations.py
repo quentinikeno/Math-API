b5e042f3efe9d3a3d92c5992d2eccf2980f5a1f1
Quentin Ikeno <ikenoq@gmail.com>
2023-05-18 18:46:20 -1000

    Test the add method with custom settings for the mins and maxes.

--- services/test_equations.py
+++ services/test_equations.py
@@ -4,7 +4,7 @@ from equations import Equations
 class TestEquations(unittest.TestCase):
     """Tests for the Equations class."""
     
-    def test_add(self):
+    def test_add_default(self):
         """Test the add method with the default settings."""
         
         result = Equations().add()
@@ -18,3 +18,18 @@ class TestEquations(unittest.TestCase):
         self.assertEqual(result["operation"], "+")
         self.assertEqual(result["expression"], f'{result["first"]} + {result["second"]}')
         self.assertEqual(result["answer"], result["first"] + result["second"])
+        
+    def test_add_custom(self):
+        """Test the add method with the min and max set to 2 and 3 for the first and second numbers."""
+        
+        result = Equations(2, 3, 2, 3).add()
+        
+        self.assertTrue("first" in result)
+        self.assertTrue(result["first"] >= 2)
+        self.assertTrue(result["first"] <= 3)
+        self.assertTrue("second" in result)
+        self.assertTrue(result["second"] >= 2)
+        self.assertTrue(result["second"] <= 3)
+        self.assertEqual(result["operation"], "+")
+        self.assertEqual(result["expression"], f'{result["first"]} + {result["second"]}')
+        self.assertEqual(result["answer"], result["first"] + result["second"]),
