a05e842e23964d738de0fe7c54e553e241285cc5
Quentin Ikeno <ikenoq@gmail.com>
2023-05-19 17:06:18 -1000

    Add method for creating subtraction problems.

--- services/equations.py
+++ services/equations.py
@@ -17,7 +17,18 @@ class Equations:
         self.negative = negative
 
     def add(self) -> Equation:
+        """Returns a dictionary with the type of the class Equation for an addtion problem."""
         operation = "+"
+        expression = f"{self.first} {operation} {self.second}"
+        answer = self.first + self.second
+        return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
+    
+    def sub(self) -> Equation:
+        """Returns a dictionary with the type of the class Equation for subtraction problem problem."""
+        operation = "-"
+        if(self.first - self.second < 0 and self.negative == False):
+            # Switch the first and second number if the difference is negative and negatives aren't allowed
+            self.first, self.second = self.second, self.first
         expression = f"{self.first} {operation} {self.second}"
         answer = self.first + self.second
         return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
