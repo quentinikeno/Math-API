eea1e9ffa7fbf1405d5a58a1452a2f0c8acc430e
quentinikeno <ikenoq@gmail.com>
2023-05-21 18:01:05 -1000

    Create method for generating division problems.

--- services/equations.py
+++ services/equations.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 defaultMax = 99
 defaultMin = 1
@@ -36,6 +36,29 @@ class Generator:
     def mul(self) -> Equation:
         """Returns a dictionary with the type of the class Equation for a multiplication problem problem."""
         operation = "*"
+        expression = f"{self.first} {operation} {self.second}"
+        answer = self.first * self.second
+        return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
+    
+    def div(self) -> Equation:
+        """Returns a dictionary with the type of the class Equation for a division problem problem."""
+        divisors = [1]
+        if self.first == 0:
+            self.first = 1
+        if self.first % self.second != 0:
+            start = self.first if self.first < 0 else 1
+            for i in range(start, int(self.first / 2) + 1):
+                if self.first % i == 0:
+                    divisors.append(i)
+            lower_divisors = divisors
+            for lower_divisor in lower_divisors:
+                lower_divisors.append(self.first / lower_divisor)
+            if self.first < 0:
+                negative_divisors = divisors
+                for negative_divisor in negative_divisors:
+                    divisors.append(negative_divisor * -1)
+            self.second = choice(divisors)
+        operation = "/"
         expression = f"{self.first} {operation} {self.second}"
         answer = self.first * self.second
         return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
