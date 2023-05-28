250923e05356c8b8abc0175e4facb29468b8b33a
Quentin Ikeno <ikenoq@gmail.com>
2023-05-27 16:42:48 -1000

    Add method to return a random math problem.

--- services/equations.py
+++ services/equations.py
@@ -18,14 +18,34 @@ class Generator:
         self.negative = negative
 
     def add(self) -> Equation:
-        """Returns a dictionary with the type of the class Equation for an addtion problem."""
+        """Returns a dictionary with the type of the class Equation for an addtion problem.
+        
+            Returns:
+                Equation: {
+                first: int
+                second: int
+                operation: str
+                expression: str
+                answer: int
+                }
+        """
         operation = "+"
         expression = f"{self.first} {operation} {self.second}"
         answer = self.first + self.second
         return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
     
     def sub(self) -> Equation:
-        """Returns a dictionary with the type of the class Equation for a subtraction problem problem."""
+        """Returns a dictionary with the type of the class Equation for a subtraction problem problem.
+        
+            Returns:
+                Equation: {
+                first: int
+                second: int
+                operation: str
+                expression: str
+                answer: int
+                }
+        """
         operation = "-"
         if(self.first - self.second < 0 and self.negative == False):
             # Switch the first and second number if the difference is negative and negatives aren't allowed
@@ -35,14 +55,34 @@ class Generator:
         return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
     
     def mul(self) -> Equation:
-        """Returns a dictionary with the type of the class Equation for a multiplication problem problem."""
+        """Returns a dictionary with the type of the class Equation for a multiplication problem problem.
+        
+            Returns:
+                Equation: {
+                first: int
+                second: int
+                operation: str
+                expression: str
+                answer: int
+                }
+        """
         operation = "*"
         expression = f"{self.first} {operation} {self.second}"
         answer = self.first * self.second
         return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
     
     def div(self) -> Equation:
-        """Returns a dictionary with the type of the class Equation for a division problem problem."""
+        """Returns a dictionary with the type of the class Equation for a division problem problem.
+        
+            Returns:
+                Equation: {
+                first: int
+                second: int
+                operation: str
+                expression: str
+                answer: int
+                }
+        """
         if self.first == 0:
             self.first = randint(defaultMin, defaultMax)
         if self.first % self.second != 0:
@@ -53,8 +93,11 @@ class Generator:
         answer = int(self.first / self.second)
         return {"first": self.first, "second": self.second, "operation": operation, "expression": expression, "answer": answer}
     
-    def find_divisors(self):
-        """Method to help find all divisors of self.first.  Will return a list of all divisors."""
+    def find_divisors(self) -> list:
+        """Method to help find all divisors of self.first.  Will return a list of all divisors.
+        
+            Returns: [int, int, int, ...]
+        """
         if self.first == 0:
             raise ValueError("Cannot find divisors for 0.")
         divisors = []
@@ -68,4 +111,26 @@ class Generator:
             for divisor in positive_divisors:
                 divisors.append(-divisor)
         return list(set(divisors))
+    
+    def random(self) -> Equation:
+        """_summary_
+
+        Returns:
+            Equation: {
+            first: int
+            second: int
+            operation: str
+            expression: str
+            answer: int
+            }
+        """
+        randNumber = randint(1, 4)
         
+        if randNumber == 1:
+            return self.add()
+        elif randNumber == 2:
+            return self.sub()
+        elif randNumber == 3:
+            return self.mul()
+        else:
+            return self.div()
