7807e31774f94b332e60f56c3b22e332b4eba542
Quentin Ikeno <ikenoq@gmail.com>
2023-05-28 18:20:05 -1000

    Add a /mul route.

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from services import equations
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def root():
     return {"msg": "Welcome to the Math API.  Go to /docs for documentation."}
 
 @app.get("/add")
-def read_item(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
+def add(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).add()
 
 @app.get("/sub")
-def read_item(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
+def sub(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond, negative).sub()
+
+@app.get("/mul")
+def mul(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
+    return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).mul()
