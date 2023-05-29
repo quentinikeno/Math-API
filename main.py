b4d4fb9ded8fc329c95cf3c60db1a17afdfd36bd
Quentin Ikeno <ikenoq@gmail.com>
2023-05-28 18:23:55 -1000

    Add a /div route.

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ from services import equations
 app = FastAPI()
 
 @app.get("/")
-def root():
+def get_root():
     return {"msg": "Welcome to the Math API.  Go to /docs for documentation."}
 
 @app.get("/add")
-def add(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
+def get_add(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).add()
 
 @app.get("/sub")
-def sub(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
+def get_sub(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond, negative).sub()
 
 @app.get("/mul")
-def mul(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
+def get_mul(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).mul()
+
+@app.get("/div")
+def get_div(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
+    return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).div()
