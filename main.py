c439699f7fe541668e174f5d0d8ca0968fa36fe0
Quentin Ikeno <ikenoq@gmail.com>
2023-05-29 12:13:47 -1000

    Add a min and max parameter to each route.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI
 from services import equations
 from pydantic import BaseModel
@@ -19,26 +20,56 @@ def get_root():
     return {"msg": "Welcome to the Math API!  Go to /docs for documentation."}
 
 @app.get("/add", response_model = Equation, responses = add_responses)
-def get_add(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
+def get_add(min: Union[int, None] = None, max: Union[int, None] = None, minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     """The addition route which responds with a random addition problem."""
+    if type(min) == int:
+       minFirst = min
+       minSecond = min
+    if type(max) == int:
+       maxFirst = max
+       maxSecond = max
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).add()
 
 @app.get("/sub", response_model = Equation, responses = sub_responses)
-def get_sub(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
+def get_sub(min: Union[int, None] = None, max: Union[int, None] = None, minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
     """The subtraction route which responds with a random subtraction problem."""
+    if type(min) == int:
+       minFirst = min
+       minSecond = min
+    if type(max) == int:
+       maxFirst = max
+       maxSecond = max
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond, negative).sub()
 
 @app.get("/mul", response_model = Equation, responses = mul_responses)
-def get_mul(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
+def get_mul(min: Union[int, None] = None, max: Union[int, None] = None, minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     """The multiplication route which responds with a random multiplication problem."""
+    if type(min) == int:
+       minFirst = min
+       minSecond = min
+    if type(max) == int:
+       maxFirst = max
+       maxSecond = max
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).mul()
 
 @app.get("/div", response_model = Equation, responses = div_responses)
-def get_div(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
+def get_div(min: Union[int, None] = None, max: Union[int, None] = None, minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     """The division route which responds with a random division problem."""
+    if type(min) == int:
+       minFirst = min
+       minSecond = min
+    if type(max) == int:
+       maxFirst = max
+       maxSecond = max
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).div()
 
 @app.get("/random", response_model = Equation, responses = random_responses)
-def get_random(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
+def get_random(min: Union[int, None] = None, max: Union[int, None] = None, minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
     """The random route which responds with an addtion, a subtraction, a multiplication, or a division problem."""
+    if type(min) == int:
+       minFirst = min
+       minSecond = min
+    if type(max) == int:
+       maxFirst = max
+       maxSecond = max
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond, negative).random()
