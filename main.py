bae1584480e360c477a834981be676a250fe2c60
Quentin Ikeno <ikenoq@gmail.com>
2023-05-29 11:20:28 -1000

    Add the response examples for the documentation to the main routes.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 from services import equations
 from pydantic import BaseModel
+from response_examples import add_responses, sub_responses, mul_responses, div_responses, random_responses
 
 app = FastAPI()
 
@@ -18,22 +19,22 @@ class Equation(BaseModel):
 def get_root():
     return {"msg": "Welcome to the Math API.  Go to /docs for documentation."}
 
-@app.get("/add", response_model=Equation)
+@app.get("/add", response_model = Equation, responses = add_responses)
 def get_add(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).add()
 
-@app.get("/sub", response_model=Equation)
+@app.get("/sub", response_model = Equation, responses = sub_responses)
 def get_sub(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond, negative).sub()
 
-@app.get("/mul", response_model=Equation)
+@app.get("/mul", response_model = Equation, responses = mul_responses)
 def get_mul(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).mul()
 
-@app.get("/div", response_model=Equation)
+@app.get("/div", response_model = Equation, responses = div_responses)
 def get_div(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond).div()
 
-@app.get("/random", response_model=Equation)
+@app.get("/random", response_model = Equation, responses = random_responses)
 def get_random(minFirst: int = 1, maxFirst: int = 99, minSecond: int = 1, maxSecond: int = 99, negative: bool = False):
     return equations.Generator(minFirst, maxFirst, minSecond, maxSecond, negative).random()
